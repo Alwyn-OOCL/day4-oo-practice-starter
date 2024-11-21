43a160c06b2316c391a596f706cb563dcc321ae1
HEAL <HEAL@OOCL.COM>
2024-11-21 11:21:05 +0800

    [OOP practice] pass AC1

--- src/afs/training/oo/Bus.java
+++ src/afs/training/oo/Bus.java
@@ -1,4 +1,17 @@
 package afs.training.oo;
 
 public class Bus {
+    private String name;
+
+    private Integer speed;
+
+    public Bus(String name, Integer speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public void speedUp() {
+        speed += 5;
+        System.out.println(name + ": speed up to " + speed + " km/h");
+    }
 }
